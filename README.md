d0091096a2a87bfaae696b46a1e3f812fa9d43c0
halavi <halavi@ethz.ch>
2024-02-17 15:16:22 +0330

    updated

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # WebAppAi
+This is a web application containing some simple image & text Ai tools; created with python and flask.
 
-## Deploy at render.com
+This application uses pretrained Ai models from https://huggingface.co/. Hence, you need to create a huggingface API key (free).
 
-build command: python3 -m pip install --upgrade pip && pip install -r requirements.txt
+This application is deployed with render.com accessible at https://ai-btfi.onrender.com/.
 
-start command: gunicorn app:app
+## Deploy settings (render.com)
+
+Runtime: `Python 3`
+
+Build command: `python3 -m pip install --upgrade pip && pip install -r requirements.txt`
+
+Start command: `gunicorn app:app`
 
 ### Environment Variables
+Add these environment variables: 
 
-PYTHON_VERSION    3.9.18
+`PYTHON_VERSION` =   `3.9.18`
 
-HF_API_KEY        `your hugging face API key`
+`HF_API_KEY`     =   `your hugging face API key`
--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import os
 import io
 from PIL import Image
+import random
  
 app = Flask(__name__)
  
@@ -15,22 +16,34 @@ load_dotenv()
 apikey = os.environ['HF_API_KEY']
  
 @app.route('/')
-def index():
+@app.route('/index.html')
+def homepage():
     return render_template('index.html')
 
-@app.route('/submit_text', methods=['POST'])
-def submit_text():
-    text = frequest.form['text']
-    API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-    headers = {"Authorization": f"Bearer {apikey}"}
-    def query(payload):
-        response = requests.post(API_URL, headers=headers, json=payload)
-        return response.content
-    image_bytes = query({"inputs": text})
-    image = Image.open(io.BytesIO(image_bytes))
-    save_path = os.path.join(app.config['DUMP'], 'img.jpg')
-    image.save(save_path)
-    return send_from_directory(app.config['DUMP'], 'img.jpg')
+@app.route('/text2image.html', methods=['GET', 'POST'])
+def text2image():
+    if frequest.method == 'GET':
+        return render_template("text2image.html", input_prompt=gen_random_text2image_prompt())
+    if "gen_img_btn" in frequest.form:
+        usertext = frequest.form['usertext']
+        API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+        headers = {"Authorization": f"Bearer {apikey}"}
+        def query(payload):
+            response = requests.post(API_URL, headers=headers, json=payload)
+            return response.content
+        image_bytes = query({"inputs": usertext})
+        image = Image.open(io.BytesIO(image_bytes))
+        save_path = os.path.join(app.config['DUMP'], 'img.jpg')
+        image.save(save_path)
+        return render_template("text2image.html", input_prompt=usertext, output_prompt=usertext, output_img=save_path)
+    elif "gen_txt_btn" in frequest.form:
+        return render_template("text2image.html", input_prompt=gen_random_text2image_prompt())
+
+def gen_random_text2image_prompt():
+    with open("static/materials/random_image_titles.txt", 'r') as f:
+        lines = f.readlines()
+    i = random.randint(0, len(lines)-1)
+    return lines[i]
 
 if __name__ == '__main__':
     app.run(debug=True, port=10000)
