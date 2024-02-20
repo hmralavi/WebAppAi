60dce2b5a563e3a4a138eb6d35c9f894276141c2
halavi <halavi@ethz.ch>
2024-02-20 12:12:09 +0330

    new appearance

--- app.py
+++ app.py
@@ -23,24 +23,29 @@ def homepage():
 @app.route('/text2image.html', methods=['GET', 'POST'])
 def text2image():
     if frequest.method == 'GET':
-        return render_template("text2image.html", input_prompt=gen_random_text2image_prompt())
+        print("GET GET")
+        return render_template("text2image.html")
     if "gen_img_btn" in frequest.form:
         usertext = frequest.form['usertext']
-        API_URL = "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5"
+        API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
         headers = {"Authorization": f"Bearer {apikey}"}
         def query(payload):
             response = requests.post(API_URL, headers=headers, json=payload)
             return response.content
         image_bytes = query({"inputs": usertext})
-        image = Image.open(io.BytesIO(image_bytes))
-        save_path = os.path.join(app.config['DUMP'], 'img.jpg')
-        image.save(save_path)
-        return render_template("text2image.html", input_prompt=usertext, output_prompt=usertext, output_img=save_path)
+        try:
+            image = Image.open(io.BytesIO(image_bytes))
+            save_path = os.path.join(app.config['DUMP'], 'img.jpg')
+            image.save(save_path)
+            return send_from_directory(app.config['DUMP'], 'img.jpg')
+        except:
+            return render_template("text2image.html", err_msg=f"The server is not responding. Please try again.", input_prompt=usertext)
     elif "gen_txt_btn" in frequest.form:
+        print("gen_txt_btn gen_txt_btn")
         return render_template("text2image.html", input_prompt=gen_random_text2image_prompt())
 
 def gen_random_text2image_prompt():
-    with open("static/materials/random_image_titles.txt", 'r') as f:
+    with open("static/assets/random_image_titles.txt", 'r') as f:
         lines = f.readlines()
     i = random.randint(0, len(lines)-1)
     return lines[i]
