d2eb85005bfb7d6c0244fe5a375d8dc20cbe4534
halavi <halavi@ethz.ch>
2024-02-22 23:49:00 +0330

    generate random prompt using js

--- app.py
+++ app.py
@@ -28,58 +28,54 @@ def homepage():
 def text2image():
     if frequest.method == 'GET':
         return render_template("text2image.html")
-    if "gen_img_btn" in frequest.form:
-        usertext = frequest.form['usertext']
-        API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-        headers = {"Authorization": f"Bearer {apikey}"}
-        def query(payload):
-            response = requests.post(API_URL, headers=headers, json=payload)
-            return response.content
-        try:
-            image_bytes = query({"inputs": usertext})
-            image = Image.open(io.BytesIO(image_bytes))
-            save_path = os.path.join(app.config['DUMP'], 'img.jpg')
-            image.save(save_path)
-            return send_from_directory(app.config['DUMP'], 'img.jpg')
-        except:
-            return render_template("text2image.html", err_msg=f"The server is not responding. Please try again.", input_text=usertext)
-    elif "gen_txt_btn" in frequest.form:
-        return render_template("text2image.html", input_text=gen_random_text2image_prompt())
+    usertext = frequest.form['usertext']
+    API_URL = "https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
+    headers = {"Authorization": f"Bearer {apikey}"}
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.content
+    try:
+        image_bytes = query({"inputs": usertext})
+        image = Image.open(io.BytesIO(image_bytes))
+        save_path = os.path.join(app.config['DUMP'], 'img.jpg')
+        image.save(save_path)
+        return send_from_directory(app.config['DUMP'], 'img.jpg')
+    except:
+        return render_template("text2image.html", err_msg=f"The server is not responding. Please try again.", input_text=usertext)
 
 @app.route('/textsum.html', methods=['GET', 'POST'])
 def textsum():
     if frequest.method == 'GET':
         return render_template("textsum.html", min_length=30, max_length=130)
-    if "sum_txt_btn" in frequest.form:
-        usertxt = frequest.form['usertext']
-        min_length = int(frequest.form['min_length'])
-        max_length = int(frequest.form['max_length'])
-        if 'userfile' in frequest.files:
-            docfile = frequest.files['userfile']
-            if docfile.filename != '':
-                filename = secure_filename(docfile.filename)
-                filename = 'doc' + os.path.splitext(filename)[1]
-                filepath = os.path.join(app.config['UPLOAD'], filename)
-                docfile.save(filepath)
-                usertxt = read_document(filepath)
-        if usertxt.strip() == "":
-            return render_template("textsum.html", err_msg=f"Provide a text or a file please.", input_text=usertxt, min_length=min_length, max_length=max_length)
-        usertxt_path = os.path.join(app.config['DUMP'], 'usertxt.txt')
-        with open(usertxt_path, 'w', encoding='utf-8') as file:
-            file.write(usertxt)
-        API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
-        headers = {"Authorization": f"Bearer {apikey}"}
-        def query(payload):
-            response = requests.post(API_URL, headers=headers, json=payload)
-            return response.json()
-        try:
-            sumtxt = query({"inputs": usertxt, "parameters": {"do_sample": False, "min_length": min_length, "max_length": max_length}})[0]['summary_text']
-            sumtxt_path = os.path.join(app.config['DUMP'], 'sumtext.txt')
-            with open(sumtxt_path, 'w', encoding='utf-8') as file:
-                file.write(sumtxt)
-            return render_template("textsum.html", input_text=usertxt, show_sum="show-sum", sum_txt=sumtxt, min_length=min_length, max_length=max_length)
-        except:
-            return render_template("textsum.html", err_msg=f"The server is not responding. Please try again.", input_text=usertxt, min_length=min_length, max_length=max_length)
+    usertxt = frequest.form['usertext']
+    min_length = int(frequest.form['min_length'])
+    max_length = int(frequest.form['max_length'])
+    if 'userfile' in frequest.files:
+        docfile = frequest.files['userfile']
+        if docfile.filename != '':
+            filename = secure_filename(docfile.filename)
+            filename = 'doc' + os.path.splitext(filename)[1]
+            filepath = os.path.join(app.config['UPLOAD'], filename)
+            docfile.save(filepath)
+            usertxt = read_document(filepath)
+    if usertxt.strip() == "":
+        return render_template("textsum.html", err_msg=f"Provide a text or a file please.", input_text=usertxt, min_length=min_length, max_length=max_length)
+    usertxt_path = os.path.join(app.config['DUMP'], 'usertxt.txt')
+    with open(usertxt_path, 'w', encoding='utf-8') as file:
+        file.write(usertxt)
+    API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+    headers = {"Authorization": f"Bearer {apikey}"}
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+    try:
+        sumtxt = query({"inputs": usertxt, "parameters": {"do_sample": False, "min_length": min_length, "max_length": max_length}})[0]['summary_text']
+        sumtxt_path = os.path.join(app.config['DUMP'], 'sumtext.txt')
+        with open(sumtxt_path, 'w', encoding='utf-8') as file:
+            file.write(sumtxt)
+        return render_template("textsum.html", input_text=usertxt, show_sum="show-sum", sum_txt=sumtxt, min_length=min_length, max_length=max_length)
+    except:
+        return render_template("textsum.html", err_msg=f"The server is not responding. Please try again.", input_text=usertxt, min_length=min_length, max_length=max_length)
 
 def gen_random_text2image_prompt():
     with open("static/assets/random_image_titles.txt", 'r') as f:
